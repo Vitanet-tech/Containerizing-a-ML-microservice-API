46471adf9c42868c430d068a904c827514ceb807
Vitanet-tech <vitalisibekwe@gmail.com>
2023-03-08 13:02:38 +0000

    project commit

---
+++ run_addtogithub.sh
@@ -0,0 +1,13 @@
+#To push local repo to github.
+git init
+
+git add --all
+
+git commit -m "project commit"
+
+#git remote set-url origin https://<githubtoken>@github.com/<username>/<repositoryname>.git
+git remote set-url origin https://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@github.com/Vitanet-tech/DevOps-Microservice-Kubernetes.git
+
+git branch -M main
+
+git push -u origin main
