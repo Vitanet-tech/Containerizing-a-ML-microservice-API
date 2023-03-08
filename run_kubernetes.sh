e1380a0ec3ce801924982914aa7852e67337b517
EC2 Default User <ec2-user@ip-172-31-22-105.ec2.internal>
2023-03-08 11:32:35 +0000

    initial commit

---
+++ run_kubernetes.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath=vitanetdocker/ml-microservice-api
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run vitdevops \
+	--image=$dockerpath:latest --port=80
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward vitdevops 8000:80
+
